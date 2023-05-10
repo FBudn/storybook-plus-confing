f37354807ad0fd4d6ac8e0e04f7aadb151212ac7
FBudn <111660524+FBudn@users.noreply.github.com>
2023-05-10 10:40:48 +0200

    checbkoxInput style file

---
+++ components/Atoms/CheckboxInput/CheckboxInput.style.tsx
@@ -0,0 +1,14 @@
+import { StyleSheet } from "react-native";
+
+const CheckboxInputstyles = StyleSheet.create({
+  container: {
+    maxHeight: 30,
+    width: 30,
+    maxWidth: 30,
+    minHeight: 30,
+    minWidth: 30,
+    marginLeft: 5,
+  },
+});
+
+export default CheckboxInputstyles;
--- components/Atoms/CheckboxInput/CheckboxInput.tsx
+++ components/Atoms/CheckboxInput/CheckboxInput.tsx
@@ -1,6 +1,6 @@
 import Checkbox from "expo-checkbox";
 import React from "react";
-import { StyleSheet } from "react-native";
+import CheckboxInputstyles from "./CheckboxInput.style";
 
 export interface CheckboxInputProps {
   testId: string;
@@ -8,18 +8,7 @@ export interface CheckboxInputProps {
 
 // export type CheckboxRef = HTMLInputElement;
 const CheckboxInput: React.FC<CheckboxInputProps> = ({ testId }) => {
-  return <Checkbox style={styles.container} testID={testId} />;
+  return <Checkbox style={CheckboxInputstyles.container} testID={testId} />;
 };
-
-const styles = StyleSheet.create({
-  container: {
-    maxHeight: 30,
-    width: 30,
-    maxWidth: 30,
-    minHeight: 30,
-    minWidth: 30,
-    marginLeft: 5,
-  },
-});
 
 export default CheckboxInput;
