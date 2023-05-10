dc863978e5a842202728c523a3aee6e58bcf3fa1
FBudn <111660524+FBudn@users.noreply.github.com>
2023-05-10 10:50:05 +0200

    LinkButton style file

---
+++ components/Atoms/LinkButton/LinkButton.style.tsx
@@ -0,0 +1,20 @@
+import { StyleSheet } from "react-native";
+
+const LinkButtonStyles = StyleSheet.create({
+  container: {
+    justifyContent: "flex-end",
+    alignItems: "flex-start",
+    alignContent: "flex-start",
+    textAlign: "left",
+  },
+  text: {
+    color: "rgb(82 82 91)",
+    fontWeight: "500",
+    textDecorationLine: "underline",
+    textDecorationColor: "rgb(82 82 91)",
+    textDecorationStyle: "solid",
+    fontSize: 20,
+  },
+});
+
+export default LinkButtonStyles;
--- components/Atoms/LinkButton/LinkButton.tsx
+++ components/Atoms/LinkButton/LinkButton.tsx
@@ -1,5 +1,6 @@
 import React from "react";
-import { Text, StyleSheet, TouchableOpacity, View } from "react-native";
+import { Text, TouchableOpacity, View } from "react-native";
+import LinkButtonStyles from "./LinkButton.style";
 
 export interface LinkButtonProps {
   children: React.ReactNode;
@@ -15,31 +16,14 @@ const LinkButton: React.FC<LinkButtonProps> = ({
   return (
     <View>
       <TouchableOpacity
-        style={styles.container}
+        style={LinkButtonStyles.container}
         testID={testId}
         onPress={onClickLink}
       >
-        <Text style={styles.text}> {children} </Text>
+        <Text style={LinkButtonStyles.text}> {children} </Text>
       </TouchableOpacity>
     </View>
   );
 };
-
-const styles = StyleSheet.create({
-  container: {
-    justifyContent: "flex-end",
-    alignItems: "flex-start",
-    alignContent: "flex-start",
-    textAlign: "left",
-  },
-  text: {
-    color: "rgb(82 82 91)",
-    fontWeight: "500",
-    textDecorationLine: "underline",
-    textDecorationColor: "rgb(82 82 91)",
-    textDecorationStyle: "solid",
-    fontSize: 20,
-  },
-});
 
 export default LinkButton;
